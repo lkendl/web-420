d137a0bc3b63cb012137b9e3b7f3b831932407bc
Laura Kendl <lkendl@live.com>
2022-05-10 22:20:26 -0600

    update for testing

--- app.js
+++ app.js
@@ -25,33 +25,26 @@ const teamAPI = require("./routes/kendl-team-routes");
 // Create Express application.
 let app = express();
 
-/* 
+/**
   ----- MIDDLEWARE -----
 */
-// App 'use' express.json().
 app.use(express.json());
-
-// App 'use' express.urlencoded({'extended':true});.
 app.use(express.urlencoded({'extended':true}));
 
 /**
   ----- MONGODB ATLAS CONNECTION -----
 */
 const mongoDB = 'mongodb+srv://lkendl:admin@buwebdev-cluster-1.p8egd.mongodb.net/web420DB?retryWrites=true&w=majority';
-
 mongoose.connect(mongoDB);
-
-// Add a Promise.
 mongoose.Promise = global.Promise;
-
 // Create database variable to hold connections.
 var db = mongoose.connection;
-
 // Add general error handling. Output results to console.
 db.on("error", console.error.bind(console, "MongoDB connection error"));
 db.once("open", function() {
     console.log("Application connected to MongoDB instance");
 });
+
 /* 
   ----- OBJECT LITERALS -----
 */
@@ -81,9 +74,8 @@ app.use('/api', customerAPI);
 app.use('/api', teamAPI);
 
 // Create and start the Node server.
-// app.set("port", process.env.PORT || 3000);
-// http.createServer(app).listen(app.get("port"), function() {
-//     console.log("Application started and listening on port " + app.get("port"))
-// });
+app.set("port", process.env.PORT || 3000);
+http.createServer(app).listen(app.get("port"), function() {
+    console.log("Application started and listening on port " + app.get("port"))
+});
 
-http.createServer(app).listen(app.get("port"), function() {console.log("Application started on port " + app.get("port"))});
