1c2d19272f549e47daf4c7d657a48023f6c5bf66
Laura Kendl <lkendl@live.com>
2022-05-12 09:18:27 -0600

    testing Procfile

--- app.js
+++ app.js
@@ -75,16 +75,15 @@ app.use('/api', teamAPI);
 
 // Create and start the Node server.
 app.set("port", process.env.PORT || 3000);
-
-// For avoiding Heroku $PORT error
-app.get('/'), function (req, res) {
-  let result = "App is running"
-  res.send(result);
-}.listen(app.get("port"), function() {
+http.createServer(app).listen(app.get("port"), function() {
   console.log("Application started and listening on port " + app.get("port"))
 });
 
-// http.createServer(app).listen(app.get("port"), function() {
-//     console.log("Application started and listening on port " + app.get("port"))
-// });
 
+// For avoiding Heroku $PORT error
+// app.get('/'), function (req, res) {
+//   let result = "App is running"
+//   res.send(result);
+// }.listen(app.get("port"), function() {
+//   console.log("Application started and listening on port " + app.get("port"))
+// });
