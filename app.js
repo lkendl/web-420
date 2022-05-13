9590da9805f66354b65f8a7a587d880b548803dc
Laura Kendl <lkendl@live.com>
2022-05-12 21:06:43 -0600

    cleaned up code

--- app.js
+++ app.js
@@ -74,24 +74,6 @@ app.use('/api', customerAPI);
 app.use('/api', teamAPI);
 
 // Create and start the Node server.
-
-// http.createServer(app).listen(app.get('port'), function() {
-//   console.log(`Application started and listening on port ${app.get('port')}`);
-// })
-
 app.listen(process.env.PORT || 3000, function(){
   console.log("Application started and listening on port " + app.get("port"));
 });
-
-// app.set("port", process.env.PORT || 3000);
-// http.createServer(app).listen(app.get("port"), function() {
-//   console.log("Application started and listening on port " + app.get("port"))
-// });
-
-// For avoiding Heroku $PORT error
-// app.get('/'), function (req, res) {
-//   let result = "App is running"
-//   res.send(result);
-// }.listen(app.get("port"), function() {
-//   console.log("Application started and listening on port " + app.get("port"))
-// });
